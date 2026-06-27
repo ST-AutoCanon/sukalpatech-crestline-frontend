6e0660df05aa09055c2db33c79488dcb3c77b041
Your Name <your@email.com>
2026-06-27 12:56:50 +0530

    added edit function

--- src/features/user/pages/departments/FoodBusiness/FoodPages/FoodBusinessCard.tsx
+++ src/features/user/pages/departments/FoodBusiness/FoodPages/FoodBusinessCard.tsx
@@ -151,7 +151,7 @@ const FoodBusinessCard: React.FC<Props> = ({
           onClick={() => setShowModal(true)}
           className="mt-3 text-blue-600 underline text-sm self-start"
         >
-          Update Final Status
+           Update & Assign To Project Manager
         </button>
       )}
 
--- src/features/user/pages/departments/gold_business/goldbusinessCard.tsx
+++ src/features/user/pages/departments/gold_business/goldbusinessCard.tsx
@@ -148,7 +148,7 @@ const GoldFinalBusinessCard: React.FC<Props> = ({
           onClick={() => setShowModal(true)}
           className="mt-3 text-blue-600 underline text-sm self-start"
         >
-          Update Final Status
+           Update & Assign To Project Manager
         </button>
       )}
 
